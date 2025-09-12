62e7093a76e51202718c65363af958c70439592e
Vadim Fedulov <vadim_fedulov@senla.eu>
2025-09-12 18:29:46 +0400

    Add task01

---
+++ src/main/java/task01/FactorialCalculator.java
@@ -0,0 +1,20 @@
+package task01;
+
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        int result = factorial(n);
+        System.out.print("The factorial of " + n + " is " + result);
+    }
+}
