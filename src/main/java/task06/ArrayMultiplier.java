82ca0162d0e44e31b466d3920fcd8acc4123b45d
Vadim Fedulov <vadim_fedulov@senla.eu>
2025-09-13 12:50:50 +0400

    Add task06

---
+++ src/main/java/task06/ArrayMultiplier.java
@@ -0,0 +1,23 @@
+package task06;
+
+import java.util.Arrays;
+
+// Задать массив [1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1]
+// пройти по нему циклом, и числа меньшие 6 умножить на 2;
+public class ArrayMultiplier {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
+
+        System.out.println("Modified array: " + Arrays.toString(arr));
+    }
+}
